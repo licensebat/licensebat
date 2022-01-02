0e78996bdc7c27ac10f3bbfa1c910d04a4dc76d1
Roberto Huertas <roberto.huertas@outlook.com>
2022-01-02 17:49:39 +0100

    added logos and favicons

--- licensebat-cli/src/lib.rs
+++ licensebat-cli/src/lib.rs
@@ -1,3 +1,5 @@
+#![doc(html_logo_url = "https://licensebat.com/images/not_used/logo_red_ferris.png")]
+#![doc(html_favicon_url = "https://licensebat.com/images/not_used/favicons_red/favicon.ico")]
 #![allow(clippy::module_name_repetitions)]
 #![warn(missing_docs)]
 
