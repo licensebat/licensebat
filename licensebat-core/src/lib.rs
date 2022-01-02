44c6d787d7e3a9a7090dafcec45f6a7a34d5e3b4
Roberto Huertas <roberto.huertas@outlook.com>
2022-01-02 17:49:48 +0100

    added icons

--- licensebat-core/src/lib.rs
+++ licensebat-core/src/lib.rs
@@ -1,6 +1,7 @@
+#![doc(html_logo_url = "https://licensebat.com/images/not_used/logo_red_ferris.png")]
+#![doc(html_favicon_url = "https://licensebat.com/images/not_used/favicons_red/favicon.ico")]
 #![allow(clippy::module_name_repetitions)]
 #![warn(missing_docs)]
-
 //! Core types and traits for [`licensebat-cli`].
 //!
 //! Libraries authors that want to provide [`Collector`] implmentation should use this crate.
